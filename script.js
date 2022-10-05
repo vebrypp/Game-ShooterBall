d9849c7182b23d030796ebd0c0010ee18adea997
vebryanto <vebrypp@gmail.com>
2022-10-05 12:35:07 +0700

    HTML5 Canvas Game Shooter Ball

---
+++ script.js
@@ -0,0 +1,25 @@
+import Game from './sprite/js/game.js';
+
+window.addEventListener('load', function() {
+    let w, h;
+    const canvas = document.querySelector('#myCanvas');
+    const c = canvas.getContext('2d');
+    const game = new Game(w, h);
+
+    resizeCanvas();
+
+    function resizeCanvas() {
+        w = canvas.width = window.innerWidth;
+        h = canvas.height = window.innerHeight;
+    };
+    function animationLoop() {
+        c.clearRect(0, 0, w, h);
+        game.draw(c);
+        game.update();
+        requestAnimationFrame(animationLoop);
+    };
+
+    animationLoop();
+
+    window.addEventListener('resize', resizeCanvas);
+});
